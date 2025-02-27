63d4db63a2ac03ae6f091e317d588a1897c63c9b
Andrey Olishchuk <bobroe.web@gmail.com>
2025-02-27 11:59:49 +0100

    docker compose volumes

--- docker/dag/initial.py
+++ docker/dag/initial.py
@@ -1,4 +1,4 @@
-from dagster import job, op
+from dagster import job, op, repository
 
 @op
 def hello_world(context):
@@ -7,6 +7,11 @@ def hello_world(context):
 @job
 def hello_world_job():
     hello_world()
+
+# This repository definition is crucial for Dagster to discover your jobs
+@repository
+def my_repository():
+    return [hello_world_job]
 
 if __name__ == "__main__":
     result = hello_world_job.execute_in_process()
